0877eb2f60202b5ca6275e74be3fe7a524cb8554
VKlen <vlaja_Klen@hotmail.com>
2018-05-16 01:41:17 +0200

    Added project to be tested

---
+++ Palindrome Checker/PalindromeCheck/PalindromeChecker.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeCheck
+{
+    public class PalindromeChecker
+    {
+        public static bool IsPalindrome(string value)
+        {
+            int min = 0;
+            int max = value.Length - 1;
+            while (true)
+            {
+                if (min > max)
+                {
+                    return true;
+                }
+                char a = value[min];
+                char b = value[max];
+
+                
+                while (!char.IsLetterOrDigit(a))
+                {
+                    min++;
+                    if (min > max)
+                    {
+                        return true;
+                    }
+                    a = value[min];
+                }
+
+                
+                while (!char.IsLetterOrDigit(b))
+                {
+                    max--;
+                    if (min > max)
+                    {
+                        return true;
+                    }
+                    b = value[max];
+                }
+
+                if (char.ToLower(a) != char.ToLower(b))
+                {
+                    return false;
+                }
+                min++;
+                max--;
+            }
+        }
+
+    }
+}
