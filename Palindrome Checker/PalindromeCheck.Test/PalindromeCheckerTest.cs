cbe0afd854e8497b99232a3477281efd7dd63235
VKlen <vlaja_Klen@hotmail.com>
2018-05-16 01:44:04 +0200

    Added test methods

--- Palindrome Checker/PalindromeCheck.Test/PalindromeCheckerTest.cs
+++ Palindrome Checker/PalindromeCheck.Test/PalindromeCheckerTest.cs
@@ -10,5 +10,25 @@ namespace PalindromeCheck.Test
     [TestFixture]
     public class PalindromeCheckerTest
     {
-    }  
+        
+        public void PalindromeChecker_WhenNotPalindrome_ReturnsFalse(string input)
+        {
+            bool actual = PalindromeChecker.IsPalindrome(input);
+
+            bool expected = false;
+
+            Assert.AreEqual(expected, actual);
+
+
+        }
+        
+        public void PalindromeChecker_WhenIsPalindrome_ReturnsTrue(string input)
+        {
+            bool actual = PalindromeChecker.IsPalindrome(input);
+
+            bool expected = true;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
 }
